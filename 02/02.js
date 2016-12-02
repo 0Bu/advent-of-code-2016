ed84eba2921b5a340456c51ff5aaa9bb7c188f10
olebur <olegburau@icloud.com>
2016-12-02 12:29:14 +0000

    part 2

--- 02/02.js
+++ 02/02.js
@@ -12,7 +12,7 @@ function go (instructions) {
     // inline array of 3x3 to one dimention
     //             1    2    3    4    5    6    7    8    9
     let keypad = ['00','10','20','01','11','21','02','12','22'];
-     // start at 5
+    // start at 5
     let x = 1, y = 1;
     let pin = [];
 
@@ -30,8 +30,35 @@ function go (instructions) {
     return pin.map(p => keypad.indexOf(p) + 1).join('');
 }
 
+function bathroom (instructions) {
+    const keypad = [
+      [0, 0,  1,  0, 0],
+      [0, 2,  3,  4, 0],
+      [5, 6,  7,  8, 9],
+      [0,'A','B','C',0],
+      [0, 0, 'D', 0, 0],
+    ];
+    // start at 5
+    let x = 0, y = 2;
+    let pin = [];
+
+    instructions.forEach( instruction => {
+        [...instruction].forEach(move => {
+            move === 'L' && x > 0 && keypad[y][x-1] && x--;
+            move === 'R' && x < 4 && keypad[y][x+1] && x++;
+            move === 'U' && y > 0 && keypad[y-1][x] && y--;
+            move === 'D' && y < 4 && keypad[y+1][x] && y++;
+        });
+        pin.push([x, y]);
+    });
+
+    return pin.map(p => keypad[p[1]][p[0]]).join('');
+}
+
 if (!module.parent) {
-   console.log('PIN', go(getInstructions()));
+   //console.log('PIN', go(getInstructions()));
+   console.log('PIN Bathroom', bathroom(getInstructions()));
 }
 
 module.exports.go = go;
+module.exports.bathroom = bathroom;
--- test/02.js
+++ test/02.js
@@ -2,7 +2,6 @@ const assert = require('assert'),
       underTest = require("../02/02");
 
 describe('02', () => {
-
     describe('going down -> DDDD', () => {
         it('should return 8', () => {
             assert.equal(8, underTest.go(['DDDD']));
@@ -28,5 +27,52 @@ describe('02', () => {
             assert.equal(1, underTest.go(['UUUURRRRRDRRDDDLDDDLLLLLULLLLUUUU']));
         });
     });
+});
 
+describe('02-2', () => {
+    describe('going down -> DDDD', () => {
+        it('should return 5', () => {
+            assert.equal(5, underTest.bathroom(['DDDD']));
+        });
+    });
+    describe('going up -> UUUUUU', () => {
+        it('should return 5', () => {
+            assert.equal(5, underTest.bathroom(['UUUUUU']));
+        });
+    });
+    describe('going left -> LLL', () => {
+        it('should return 5', () => {
+            assert.equal(5, underTest.bathroom(['LLL']));
+        });
+    });
+    describe('going right -> RRRRRRRRRR', () => {
+        it('should return 9', () => {
+            assert.equal(9, underTest.bathroom(['RRRRRRRRRR']));
+        });
+    });
+    describe('test all boundaries clockwise -> DRDDRDD', () => {
+        it('should return D', () => {
+            assert.equal('D', underTest.bathroom(['DRDDRDD']));
+        });
+    });
+    describe('test all boundaries clockwise -> DRDDRDDRRURRR', () => {
+        it('should return C', () => {
+            assert.equal('C', underTest.bathroom(['DRDDRDDRRURRR']));
+        });
+    });
+    describe('test all boundaries clockwise -> DRDDRDDRRURRRUR', () => {
+        it('should return 9', () => {
+            assert.equal('9', underTest.bathroom(['DRDDRDDRRURRRUR']));
+        });
+    });
+    describe('test all boundaries clockwise -> DRDDRDDRRURRRURLUUUU', () => {
+        it('should return 4', () => {
+            assert.equal('4', underTest.bathroom(['DRDDRDDRRURRRURLUUUU']));
+        });
+    });
+    describe('test all boundaries clockwise -> DRDDRDDRRURRRURLUUUULUURRLLLLL', () => {
+        it('should return 1', () => {
+            assert.equal('1', underTest.bathroom(['DRDDRDDRRURRRURLUUUULUURRLLLLL']));
+        });
+    });
 });
