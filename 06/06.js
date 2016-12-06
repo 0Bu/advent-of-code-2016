e77ca15942dbd161509f851ed5661abab1776194
olebur <olegburau@icloud.com>
2016-12-06 10:24:05 +0000

    06

--- 06/06.js
+++ 06/06.js
@@ -16,26 +16,35 @@ function* row(lines) {
 }
 
 // returns a bigram of the frequent char and frequency, e.g ['a', 5]
-function gram(str) {
+function gram(str, order) {
     let char = str.charAt(0);
-    let frequency = str.match(new RegExp(char, 'g')).length;
-    let remainder = str.replace(char,''); // remove already checked char from string
+    let re = new RegExp(char, 'g');
+    let frequency = str.match(re).length;
+    let remainder = str.replace(re,''); // remove already checked char from string
     if (remainder) {
-        let [c,f] = gram(remainder);
-        return frequency > f ? [char, frequency] : [c,f];
+        let [c,f] = gram(remainder, order);
+        return frequency * order > f * order ? [char, frequency] : [c,f];
     }
     return [char, frequency];
 }
 
-function message(lines) {
+// oder === 1 ? 'most frequest letter' : 'less frequent letter'
+function message(lines, order) {
     // iterate every row and get the frequent char
-    return [...row(lines)].map(line => gram(line)[0]).join('');
+    return [...row(lines)].map(line => gram(line, order)[0]).join('');
 }
 
 if(!module.parent) {
     console.time('time');
-    console.log('message', message(data()));
+    console.log('the error-corrected version of the message is "%s"', message(data(), 1));
     console.timeEnd('time');
+
+    console.time('time');
+    console.log('the original message that Santa is trying to send is "%s"', message(data(), -1));
+    console.timeEnd('time');
+
+    //console.log(gram('aaabbc', 1));
+    console.log(gram('aaabbbbbccccc', -1));
 }
 
 module.exports.gram = gram;
--- test/06.js
+++ test/06.js
@@ -2,12 +2,25 @@ const assert = require('assert'),
       underTest = require("../06/06");
 
 describe('06', () => {
-    describe('the frequent letter in the string "aabcddea"', function() {
+    describe('the most frequent letter in the string "aaaabbbccd"', function() {
+        let bigram = underTest.gram('aaaabbbccd', 1);
+
         it('should be a', () => {
-            assert.equal('a', underTest.gram('aabcddea')[0]);
+            assert.equal('a', bigram[0]);
         });
         it('with the frequency of 3', () => {
-            assert.equal(3, underTest.gram('aabcddea')[1]);
+            assert.equal(4, bigram[1]);
+        });
+    });
+
+    describe('the less frequent letter in the string "aaaabbbccd"', function() {
+        let bigram = underTest.gram('aaaabbbccd', -1);
+
+        it('should be d', () => {
+            assert.equal('d', bigram[0]);
+        });
+        it('with the frequency of 1', () => {
+            assert.equal(1, bigram[1]);
         });
     });
 });
