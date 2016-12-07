1efd3c191877153307152752f4c8ac38de04adb1
olebur <olegburau@icloud.com>
2016-12-07 14:18:28 +0000

    part 2

--- 07/07.js
+++ 07/07.js
@@ -8,7 +8,7 @@ function data() {
     return input.split('\n');
 }
 
-function isAbba(s) {
+function is_abba(s) {
     for(let i = 0; i < s.length; i += 1) {
         let [a,b,c,d] = [...s.substr(i,4)];
         if(a + b === d + c) return a != b;
@@ -16,21 +16,53 @@ function isAbba(s) {
     return false;
 }
 
-function ips(data) {
+function tls(data) {
     return data.filter(str => {
         let abba = false;
         let hypernet = false;
         str.split(/[\[\]]/).forEach((s, i) => {
-            if(i % 2 === 0 && !abba) abba = isAbba(s);
-            if(i % 2 && !hypernet) hypernet = isAbba(s);
+            if(i % 2 === 0 && !abba) abba = is_abba(s);
+            if(i % 2 && !hypernet) hypernet = is_abba(s);
         });
         return abba && !hypernet;
     });
 }
 
-if(!module.parent) {
-    console.log('%d IPs support TLS', ips(data()).length);
+// returns BABs of ABAs or empty array
+function get_babs(s) {
+    let babs = [];
+    for(let i = 0; i < s.length; i += 1) {
+        let [a,b,c] = s.substr(i,3);
+        if(a === c && a !== b) {
+            babs.push(b + a + b);
+        }
+    }
+    return babs;
 }
 
-module.exports.isAbba = isAbba;
-module.exports.ips = ips;
+function ssl(data) {
+    return data.filter(str => {
+        let babs = []; // array of BABs
+        let hypernets = []; // array of hypernet
+        str.split(/[\[\]]/).forEach((s, i) => {
+            if(i % 2 === 0) babs = [...babs, ...get_babs(s)];
+            if(i % 2) hypernets.push(s);
+        });
+        let h = hypernets.join('|');
+        for(let bab of babs) {
+            if (h.includes(bab)) return true;
+        }
+        return false;
+    });
+}
+
+
+if(!module.parent) {
+    console.log('%d IPs support TLS', tls(data()).length);
+    console.log('%d IPs support SSL', ssl(data()).length);
+}
+
+module.exports.is_abba = is_abba;
+module.exports.tls = tls;
+module.exports.get_babs = get_babs;
+module.exports.ssl = ssl;
--- test/07.js
+++ test/07.js
@@ -4,24 +4,45 @@ const assert = require('assert'),
 describe('07', () => {
     describe('the string "abba"', function() {
         it('should be Autonomous Bridge Bypass Annotation (ABBA)', () => {
-            assert.ok(underTest.isAbba('abba'));
+            assert.ok(underTest.is_abba('abba'));
         });
     });
     describe('the string "aaaa"', function() {
         it('should note be ABBA', () => {
-            assert.ok(!underTest.isAbba('aaaa'));
+            assert.ok(!underTest.is_abba('aaaa'));
         });
     });
-
-    describe('the number of ips', function() {
+    describe('the number of TLS IPs', function() {
         it('should be 2', () => {
-            assert.equal(3, underTest.ips([
+            assert.equal(3, underTest.tls([
                 'abba[mnop]qrst', // tls
                 'abcd[bddb]xyyx', // not tls
                 'aaaa[qwer]tyui', // not tls
                 'ioxxoj[asdfgh]zxcvbn', // tls
                 'abcddc[xyzab]abyzd[abyz]yzab', // tls
                 'abcddc[xyzab]abyzd[abba]yzab' // not tls
+            ]).length);
+        });
+    });
+    describe('the string "aba"', function() {
+        it('should return an array with "bab"', () => {
+            assert.equal('bab', underTest.get_babs('aba')[0]);
+        });
+    });
+    describe('the string "abazzzxyx"', function() {
+        it('should return an array with ["bab","yxy"]', () => {
+            assert.deepEqual(['bab','yxy'], underTest.get_babs('abazzzxyx'));
+        });
+    });
+    describe('the number of SSL IPs', function() {
+        it('should be 4', () => {
+            assert.equal(4, underTest.ssl([
+                'aba[bab]xyz', // ssl
+                'xyx[xyx]xyx', // not ssl
+                'zazbz[bzb]cdb', // ssl
+                'abcba[xyz]efgigfe[uvigivu]yzab', // ssl
+                'abzcba[xycbczab]efbcbfe[uvigivu]yzab', // ssl
+                'abzcba[xycbczab]efbbfe[uvigivu]yzab' // not ssl
             ]).length);
         });
     });
