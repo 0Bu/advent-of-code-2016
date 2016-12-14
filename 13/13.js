781fd3b4bba442c016229f47ff0f316b6a850ea4
olebur <olegburau@icloud.com>
2016-12-14 08:27:12 +0000

    13

--- 13/13.js
+++ 13/13.js
@@ -3,15 +3,15 @@
 const chalk = require('chalk'),
     arrow = chalk.bold.red,
     number = 1362, // magic input
-    X = 31*2, // x asis of the building
-    Y = 39*2, // y --
+    X = 31*2, // x axis of the building
+    Y = 50, // y --
     binom = (x,y) => x*x + 3*x + 2*x*y + y + y*y,
     ones = (i) => (i).toString(2).match(/1/g).length, // count 1s in bin number
     os = (x, y) => !(ones(binom(x,y) + number) % 2); // open space == 0
 
 let min = 5000, // random max initial value for min path length
     path = 0, // path length
-    locations = 0, // part 2, how many location can be reached in 50 steps
+    locations = new Set(), // part 2, how many location can be reached in 50 steps
     building = []; // office
 
 function fill() {
@@ -29,9 +29,9 @@ function display() {
 }
 
 function go(y,x) {
+    if(path < 51) locations.add([x,y].join(','));
     if(x === 31 && y === 39 && min > path) { min = path; display(); console.log(`new min path: ${min}\n`); }
     path += 1;
-    if(path === 50) locations += 1;
     if(building[y+1] && building[y+1][x] === '.') { building[y][x] = arrow('↓'); go(y+1,x); }
     if(building[y][x+1] === '.') { building[y][x] = arrow('→'); go(y,x+1); }
     if(building[y][x-1] === '.') { building[y][x] = arrow('←'); go(y,x-1); }
@@ -44,7 +44,7 @@ if(!module.parent) {
     fill();
     go(1,1);
     console.log('Min path to 31,39:', min);
-    console.log('50 steps distant locations:', locations);
+    console.log('50 steps distant locations:', locations.size);
 }
 
 module.exports.binom = binom;
